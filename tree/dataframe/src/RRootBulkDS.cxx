062c86883f9d0abd827626a5c8703d44f15dab18
Zhe Zhang <zzxuanyuan@gmail.com>
2019-02-06 20:11:52 -0600

    Add support for 1/2/4/8-byte data types in TBufferMgr

--- tree/dataframe/src/RRootBulkDS.cxx
+++ tree/dataframe/src/RRootBulkDS.cxx
@@ -32,11 +32,21 @@ private:
    // This is the mapping from col # to buffer.
    std::vector<TBufferFile*> fBufferMap;
 
-   // These are all the buffers that must be advanced by 4 bytes for each event.
+   // This is the mapping from col # to data type.
+   std::vector<EDataType> fDataTypeMap;
+
+   // These are all the buffers that must be advanced by n bytes for each event.
+   // 1 byte for bool/char, 2 bytes for short, 4 bytes for int/float and 8 bytes for double/long64
+   std::vector<TBufferFile*> fOneByteBuffers;
+   std::vector<TBufferFile*> fTwoByteBuffers;
    std::vector<TBufferFile*> fFourByteBuffers;
-   // A list of 4-byte values that are the targets of the void* pointer handed back to
-   // the RDF.  Each event we advance, we must
+   std::vector<TBufferFile*> fEightByteBuffers;
+   // A list of n-byte values that are the targets of the void* pointer handed back to
+   // the RDF.  Each event advances n bytes as the above buffers.
+   std::vector<Char_t*>      fOneByteValues;
+   std::vector<Short_t*>     fTwoByteValues;
    std::vector<Int_t*>       fFourByteValues;
+   std::vector<Long64_t*>    fEightByteValues;
 
    // The current absolute entry in the TTree.
    ULong64_t fCurAbsEntry{0};
@@ -66,15 +76,37 @@ public:
             printf("Skipping branch %s as we failed to retrieve the expected type info.\n", br->GetName());
             continue;
          }
-         if (dt == kFloat_t || dt == kInt_t || dt == kUInt_t) {
+         fDataTypeMap.push_back(dt);
+         if (dt == kChar_t || dt == kUChar_t || dt == kBool_t) {
+            TBufferFile *bf = new TBufferFile(TBuffer::kWrite, 32*1024);
+            fOneByteBuffers.push_back(bf);
+            fBufferMap.back() = fOneByteBuffers.back();
+            Char_t *value = new Char_t[1];
+            fOneByteValues.push_back(value);
+            fAddressMap.back() = fOneByteValues.back();
+         } else if (dt == kShort_t || dt == kUShort_t) {
+            TBufferFile *bf = new TBufferFile(TBuffer::kWrite, 32*1024);
+            fTwoByteBuffers.push_back(bf);
+            fBufferMap.back() = fTwoByteBuffers.back();
+            Short_t *value = new Short_t[1];
+            fTwoByteValues.push_back(value);
+            fAddressMap.back() = fTwoByteValues.back();
+         } else if (dt == kFloat_t || dt == kInt_t || dt == kUInt_t) {
             TBufferFile *bf = new TBufferFile(TBuffer::kWrite, 32*1024);
             fFourByteBuffers.push_back(bf);
             fBufferMap.back() = fFourByteBuffers.back();
             Int_t *value = new Int_t[1];
             fFourByteValues.push_back(value);
             fAddressMap.back() = fFourByteValues.back();
+         } else if (dt == kDouble_t || dt == kLong64_t || dt == kULong64_t) {
+            TBufferFile *bf = new TBufferFile(TBuffer::kWrite, 32*1024);
+            fEightByteBuffers.push_back(bf);
+            fBufferMap.back() = fEightByteBuffers.back();
+            Long64_t *value = new Long64_t[1];
+            fEightByteValues.push_back(value);
+            fAddressMap.back() = fEightByteValues.back();
          } else {
-            printf("Skipping branch %s as its data type (%d) is not exactly 4 bytes.\n", br->GetName(), dt);
+            printf("Skipping branch %s as its data type (%d) is not supported.\n", br->GetName(), dt);
          }
       }
    }
@@ -84,7 +116,19 @@ public:
          if (fBufferMap[idx]) delete fBufferMap[idx];
       }
       for (auto idx : ROOT::TSeqU(fAddressMap.size())) {
-         if (fAddressMap[idx]) delete fAddressMap[idx];
+         if (fAddressMap[idx]) {
+            if (fDataTypeMap[idx] == kChar_t || fDataTypeMap[idx] == kUChar_t || fDataTypeMap[idx] == kBool_t) {
+               delete static_cast<Char_t*>(fAddressMap[idx]);
+            } else if (fDataTypeMap[idx] == kShort_t || fDataTypeMap[idx] == kUShort_t) {
+               delete static_cast<Short_t*>(fAddressMap[idx]);
+            } else if (fDataTypeMap[idx] == kFloat_t || fDataTypeMap[idx] == kInt_t || fDataTypeMap[idx] == kUInt_t) {
+               delete static_cast<Int_t*>(fAddressMap[idx]);
+            } else if (fDataTypeMap[idx] == kDouble_t || fDataTypeMap[idx] == kLong64_t || fDataTypeMap[idx] == kULong64_t) {
+               delete static_cast<Long64_t*>(fAddressMap[idx]);
+            } else {
+               printf("Unknown type\n");
+            }
+         }
       }
    }
 
@@ -97,11 +141,39 @@ public:
       if (R__unlikely(fCurAbsEntry != entry)) {
           return false;
       }
-      for (auto idx : ROOT::TSeqU(fFourByteBuffers.size())) {
-         Int_t *raw_buffer = reinterpret_cast<Int_t*>(fBufferMap[idx]->GetCurrent());
-         Int_t tmp = *reinterpret_cast<Int_t*>(&raw_buffer[fCurRelEntry]);
-         char *tmp_ptr = reinterpret_cast<char *>(&tmp);
-         frombuf(tmp_ptr, fFourByteValues[idx]);
+      auto idx1b = 0;
+      auto idx2b = 0;
+      auto idx4b = 0;
+      auto idx8b = 0;
+      for (UInt_t idx = 0; idx < fBufferMap.size(); ++idx) {
+         EDataType dt = fDataTypeMap[idx];
+         if (dt == kChar_t || dt == kUChar_t || dt == kBool_t) {
+            Char_t *raw_buffer = reinterpret_cast<Char_t*>(fBufferMap[idx]->GetCurrent());
+            Char_t tmp = *reinterpret_cast<Char_t*>(&raw_buffer[fCurRelEntry]);
+            char *tmp_ptr = reinterpret_cast<char *>(&tmp);
+            frombuf(tmp_ptr, fOneByteValues[idx1b]);
+            idx1b++;
+         } else if (dt == kShort_t || dt == kUShort_t) {
+            Short_t *raw_buffer = reinterpret_cast<Short_t*>(fBufferMap[idx]->GetCurrent());
+            Short_t tmp = *reinterpret_cast<Short_t*>(&raw_buffer[fCurRelEntry]);
+            char *tmp_ptr = reinterpret_cast<char *>(&tmp);
+            frombuf(tmp_ptr, fTwoByteValues[idx2b]);
+            idx2b++;
+         } else if (dt == kFloat_t || dt == kInt_t || dt == kUInt_t) {
+            Int_t *raw_buffer = reinterpret_cast<Int_t*>(fBufferMap[idx]->GetCurrent());
+            Int_t tmp = *reinterpret_cast<Int_t*>(&raw_buffer[fCurRelEntry]);
+            char *tmp_ptr = reinterpret_cast<char *>(&tmp);
+            frombuf(tmp_ptr, fFourByteValues[idx4b]);
+            idx4b++;
+         } else if (dt == kDouble_t || dt == kLong64_t || dt == kULong64_t) {
+            Long64_t *raw_buffer = reinterpret_cast<Long64_t*>(fBufferMap[idx]->GetCurrent());
+            Long64_t tmp = *reinterpret_cast<Long64_t*>(&raw_buffer[fCurRelEntry]);
+            char *tmp_ptr = reinterpret_cast<char *>(&tmp);
+            frombuf(tmp_ptr, fEightByteValues[idx8b]);
+            idx8b++;
+         } else {
+            printf("Unknown data type %d.\n", dt);
+         }
       }
       fCurRelEntry++;
       fCurAbsEntry++;
